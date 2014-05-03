0be67e435a15b14b74adf24a3ba77371b4d66988
vas <vas@vas-MSI>
2014-05-04 01:22:54 +0400

    цц

--- GitTestProject/src/ru/vaszol/GitTestProj/main.java
+++ GitTestProject/src/ru/vaszol/GitTestProj/main.java
@@ -3,7 +3,7 @@ package ru.vaszol.GitTestProj;
 public class main {
 
 	public static void main(String[] args) {
-		System.out.println("Hello, from git hub!");
+		System.out.println("Hello, from git hub!!!!");
 
 	}
 
